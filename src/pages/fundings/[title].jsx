6307fb757001ffa286d17869d8da5cb02752874f
0xdagarn <sgnoo@naver.com>
2023-07-31 20:58:54 +0900

    Add funding success modal

--- src/pages/fundings/[title].jsx
+++ src/pages/fundings/[title].jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Router from "next/router";
 
 import { ethers } from "ethers";
 import {
@@ -11,7 +12,48 @@ import {
 import DocumentaryProducerPassABI from "../../contracts/abis/DocumentaryProducerPassABI.json";
 import { DocumentaryProducerPassAddress } from "../../contracts/address.json";
 
-const Modal = ({ isOpenModal, setIsOpenModal, write }) => {
+const FundingSuccessModal = ({ isOpenModal }) => {
+  return (
+    <div>
+      {isOpenModal && (
+        <div>
+          <div
+            className="relative z-10"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div class="flex flex-col items-center justify-center p-6 bg-[#081521]">
+                    <Image
+                      src="/dummy/funding-success-modal.png"
+                      width={680}
+                      height={933}
+                      alt="Picture of the author"
+                    />
+                    <button
+                      class="bg-[#ffcd4e] rounded-[56px] px-[16px] py-[24px] text-18 font-bold uppercase"
+                      onClick={() => {
+                        Router.replace("/scenes/tears-of-the-antarctic");
+                      }}
+                    >
+                      Go to behind the scene
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const FundingModal = ({ isOpenModal, setIsOpenModal, write }) => {
   return (
     <div>
       {isOpenModal && (
@@ -52,8 +94,10 @@ const Modal = ({ isOpenModal, setIsOpenModal, write }) => {
   );
 };
 
-export default function Home() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+export default function Funding() {
+  const [isOpenFundingModal, setIsOpenFunginModal] = useState(false);
+  const [isOpenFundingSuccessModal, setIsOpenFunginSuccessModal] =
+    useState(false);
 
   const price = ethers.parseEther("0.1");
 
@@ -73,18 +117,15 @@ export default function Home() {
 
   useEffect(() => {
     if (isSuccess) {
-      alert("success");
-      setIsOpenModal(false);
+      setIsOpenFunginModal(false);
+      setIsOpenFunginSuccessModal(true);
     }
   }, [isSuccess]);
 
   return (
     <div class="bg-[#081521] pb-[400px]">
-      <Modal
-        isOpenModal={isOpenModal}
-        setIsOpenModal={setIsOpenModal}
-        write={write}
-      />
+      <FundingModal isOpenModal={isOpenFundingModal} write={write} />
+      <FundingSuccessModal isOpenModal={isOpenFundingSuccessModal} />
       <div class="bg-[url('/bg-funding-detail.svg')] bg-cover flex items-center justify-center h-[392px] font-normal text-[64px] text-white">
         Tears of the Antarctic
       </div>
@@ -108,7 +149,7 @@ export default function Home() {
           <button
             class="bg-[#ffcd4e] rounded-[56px] px-[16px] py-[24px] text-18 font-bold uppercase mt-[-35px]"
             onClick={() => {
-              setIsOpenModal(true);
+              setIsOpenFunginModal(true);
             }}
           >
             SUPPORT THIS PROJECT
