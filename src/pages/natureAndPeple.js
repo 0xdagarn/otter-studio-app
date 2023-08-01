cce7d6e9a9f8b6a1ee891605a0211371df0839d1
0xdagarn <sgnoo@naver.com>
2023-08-01 17:10:29 +0900

    Fix build error

--- src/pages/natureAndPeple.js
+++ src/pages/natureAndPeple.js
@@ -1,12 +1,12 @@
-// images.js
-import image1 from './asset/nature_and_man_1.svg';
-import image2 from './asset/nature_and_man_2.svg';
-import image3 from './asset/nature_and_man_3.svg';
-import image4 from './asset/nature_and_man_4.svg';
+// // images.js
+// import image1 from './asset/nature_and_man_1.svg';
+// import image2 from './asset/nature_and_man_2.svg';
+// import image3 from './asset/nature_and_man_3.svg';
+// import image4 from './asset/nature_and_man_4.svg';
 
-export default [
-    image1,
-    image2,
-    image3,
-    image4,
-];
+// export default [
+//     image1,
+//     image2,
+//     image3,
+//     image4,
+// ];
